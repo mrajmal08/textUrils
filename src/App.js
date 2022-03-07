563c7550c7b91ed24aed9d8508fb5a450ee00448
mrajmalg08 <ajmalg08@gmail.com>
2022-03-07 12:23:46 +0500

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [enableMode, setenableMode] = useState('Enable dark Mode');
+
+  const toggleMode = () => {
+    if(mode == 'light'){
+      setMode('dark');
+      setenableMode('Enable Light Mode')
+      document.body.style.backgroundColor= '#282b66'
+    }else{
+      setMode('light');
+      setenableMode('Enable Dark Mode')
+      document.body.style.backgroundColor= 'white'
+    }
+  }
+
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">TextUrils</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    
+      <Navbar title="TextUrils" mode={mode} toggleMode={toggleMode} enableMode={enableMode}/>
+      {/* <Navbar /> */}
+
+      {/* <About /> */}
+
+      <div className="container my-3">
+        <TextForm heading ="Enter text to finalize" mode={mode} toggleMode={toggleMode}/>
+      </div>
+
     </>
-
-
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+
+    //convert to upper letters
+    const handleUpClick = () => {
+        let newtext = text.toUpperCase();
+        setText(newtext);
+    }
+
+    //convert to lower letter
+    const handleLoClick = () => {
+        let newtext = text.toLowerCase();
+        setText(newtext);
+    }
+
+    //clear all text
+    const handleClearCick = () => {
+        let newtext = "";
+        setText(newtext);
+    }
+
+    //capital first letter
+    const capitalFirstLetter = ()=>{
+        let words = text.split(" ")
+            let uppercaseword = ' '
+            words.forEach(element => {
+            uppercaseword += element.charAt(0).toUpperCase() + element.slice(1) + " "
+            });
+        setText(uppercaseword)
+    }
+
+    //remove all the symbols
+        const handletextExtract =()=>{
+            const regex = /[0-9/A-Z/a-z/ /]/g;
+            const letters = text.match(regex);
+            const res1 = letters.join('');
+            setText(res1)
+        };
+
+
+    //to extract only the numbers in the text:
+    const handleNumExtract =()=>{
+
+        const regex = /[0-9/ /]/g;
+        const digits = text.match(regex);
+        const res = digits.join('');
+        setText(res)
+
+        };
+
+    //to copy text:
+    const handleCopyText =()=>{
+       let text = document.getElementById("mybox");
+       text.select();
+       navigator.clipboard.writeText(text.value);
+        };
+
+    //handle extra spaces
+    const handleExtraSpaces =()=>{
+
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+ 
+         };
+
+
+
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+
+    }
+
+const [text, setText] = useState('') 
+
+  return (
+    <>
+    <div className="container" style={{ color: props.mode=='dark'? 'white': 'black' }}>
+        <div className="mb-3">
+            <h1 >{props.heading}</h1>
+            <textarea className="form-control" style={{backgroundColor: props.mode=='dark'? 'grey': 'white', color: props.mode=='dark'? 'white': 'black'}} value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary btn-sm mx-1" onClick={handleUpClick}>Convert to Upercase</button>
+        <button className="btn btn-success btn-sm mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-info btn-sm mx-1" onClick={capitalFirstLetter}>Capital first Letter</button>
+        <button className="btn btn-warning btn-sm mx-1" onClick={handleNumExtract}>extract all numbers</button>
+        <button className="btn btn-secondary btn-sm mx-1" onClick={handleCopyText}>Copy Text</button>
+        <button className="btn btn-secondary btn-sm mx-1" onClick={handleExtraSpaces}>Hanlde Extra Spaces</button>
+        <button className="btn btn-danger btn-sm mx-1" onClick={handleClearCick}>Clear Text</button>
+
+
+        </div>
+        <div className="container my-3" style={{ color: props.mode=='dark'? 'white': 'black' }}>
+            <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} Characters</p>
+                <p>{0.008 *text.split(" ").length} per read</p>
+            <h2>Preview</h2>
+             <p>{text.length>0? text: "Enter something above textfeild to preview in it"}</p>
+        </div>
+    </>
+  )
+}
